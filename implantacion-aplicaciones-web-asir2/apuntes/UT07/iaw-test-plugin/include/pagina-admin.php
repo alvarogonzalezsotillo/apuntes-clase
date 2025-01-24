7522ee0f7a01db820e79ec8bc8e1670267a26c33
alvaro@thinkpad <alvaro@thinkpad>
2025-01-24 13:00:54 +0100

    UT07, inicio

---
+++ implantacion-aplicaciones-web-asir2/apuntes/UT07/iaw-test-plugin/include/pagina-admin.php
@@ -0,0 +1,19 @@
+<div class="wrap">
+     <h1>Hello!</h1>
+     <p>Es una página informativa del plugin de disclaimer. La de settings está en otro sitio</p>
+     <p>Ejemplo de uso de flags para saber qué sistema está viendo esta página:</p>
+
+
+     is_iphone <?= $is_iphone ?> <br>
+     is_chrome <?= $is_chrome ?> <br>
+     is_safari <?= $is_safari ?> <br>
+     is_NS4 <?= $is_NS4 ?> <br>
+     is_opera <?= $is_opera ?> <br>
+     is_macIE <?= $is_macIE ?> <br>
+     is_winIE <?= $is_winIE ?> <br>
+     is_gecko <?= $is_gecko ?> <br>
+     is_lynx <?= $is_lynx ?> <br>
+     is_IE <?= $is_IE ?> <br>
+     is_edge <?= $is_edge ?> <br>
+
+</div>
