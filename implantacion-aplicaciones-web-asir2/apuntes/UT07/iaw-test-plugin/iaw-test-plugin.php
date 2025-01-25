0efe54427a99be97144b90b1e3bcca2c6c4fb4c5
alvaro@thinkpad <alvaro@thinkpad>
2025-01-25 19:38:15 +0100

    ut07 en curso

--- implantacion-aplicaciones-web-asir2/apuntes/UT07/iaw-test-plugin/iaw-test-plugin.php
+++ implantacion-aplicaciones-web-asir2/apuntes/UT07/iaw-test-plugin/iaw-test-plugin.php
@@ -16,10 +16,10 @@ require_once plugin_dir_path(__FILE__) . 'include/lib.php';
 
 
 add_action( 'admin_menu', 'iaw_add_sys_info_to_admin_link' );
-add_action('wp_enqueue_scripts', 'iaw_add_resources_to_page');
-add_filter('the_content', 'iaw_add_disclaimer');
-add_action('wp_dashboard_setup', 'iaw_custom_dashboard_widget');
+add_action( 'wp_enqueue_scripts', 'iaw_add_resources_to_page' );
+add_filter( 'the_content', 'iaw_add_disclaimer' );
+add_action( 'wp_dashboard_setup', 'iaw_custom_dashboard_widget' );
 
 
-add_action('admin_init', 'iaw_custom_plugin_settings');
+add_action( 'admin_init', 'iaw_custom_plugin_settings' );
 
